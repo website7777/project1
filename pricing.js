3b59178864fb22b1c11dc093a1fd316dd7f279b8
website7777 <95430611+website7777@users.noreply.github.com>
2023-08-18 21:38:13 +0300

    Add files via upload

---
+++ pricing.js
@@ -0,0 +1,13 @@
+// Button click actions
+document.getElementById("button1").addEventListener("click", function() {
+    window.location.href = "https://www.google.com/maps/@59.3775844,28.1871153,21z?entry=ttu";
+  });
+  
+  document.getElementById("button2").addEventListener("click", function() {
+    window.location.href = "https://www.youtube.com/";
+  });
+  
+  document.getElementById("button3").addEventListener("click", function() {
+    window.location.href = "https://www.google.com/";
+  });
+  
